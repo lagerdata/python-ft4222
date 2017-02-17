c0a1f3b8c7797de9e6d99c124f8aecc2b1652e61
Martin Gysel <m.gysel@msr.ch>
2017-02-17 15:30:47 +0100

    add script to build in linux

---
+++ build-linux.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+unamem=$(uname -m)
+
+case "$unamem" in
+	x86_64) platform="x86_64" ;;
+	i386|i686) platform="i386" ;;
+	armv6*|armv7*) platform="arm-v6-hf" ;;
+	*) echo "Libft4222 is not currently supported on '$unamem'."; exit 1 ;;
+esac
+
+CFLAGS=-Ilinux LDFLAGS=-Llinux/build-${platform}/ python setup.py build_ext -i
