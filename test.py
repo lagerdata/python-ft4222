fac3a182cfb241b4ec3d532f5b70177cacb43bee
Martin Gysel <m.gysel@msr.ch>
2017-02-23 21:07:45 +0100

    improve simple test a little bit

--- test.py
+++ test.py
@@ -11,27 +11,42 @@
 # |___|  |___|  |___|  |_________________/   |___|       \____\
 #
 
-import ft4222;
+import sys
+import ft4222
 
 nbDev = ft4222.createDeviceInfoList()
 print("nb of fdti devices: {}".format(nbDev))
 
 ftDetails = []
 
-if nbDev > 0:
-    print("devices:")
-    for i in range(nbDev):
-        detail = ft4222.getDeviceInfoDetail(i, False)
-        print(" - {}".format(detail))
-        ftDetails.append(detail)
-        
+if nbDev <= 0:
+    print("no devices found...")
+    sys.exit(0)
+
+
+print("devices:")
+for i in range(nbDev):
+    detail = ft4222.getDeviceInfoDetail(i, False)
+    print(" - {}".format(detail))
+    ftDetails.append(detail)
 
 #dev = ft4222.open_by_description(ftDetails[0]['description'])
 dev = ft4222.openByLocation(ftDetails[0]['location'])
 print(dev)
 
-dev.i2cMaster_Init()
+dev.i2cMaster_Init(250)
 
 res = dev.i2cMaster_Read(2, 1)
 print(res)
 
+res = dev.i2cMaster_Write(2, bytes([1,2,3,4]))
+print(res)
+
+res = dev.i2cMaster_Read(2, 1)
+print(res)
+
+res = dev.i2cMaster_Write(2, bytes([2]*7))
+print(res)
+
+res = dev.i2cMaster_Read(2, 4)
+print(res)
