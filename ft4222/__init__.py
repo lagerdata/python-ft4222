861028cabcc2191ac931e47bb88364dfb80f8e1f
Martin Gysel <me@bearsh.org>
2017-03-01 14:02:23 +0100

    add module doc string

--- ft4222/__init__.py
+++ ft4222/__init__.py
@@ -1,3 +1,8 @@
+"""Control FTDI USB chips for I2C/SPI/GPIO.
+Open a handle using one of the ft4222.openBy... functions and use the methods
+on the object thus returned.
+"""
+
 from ft4222.ft4222 import *
 from ft4222 import *
 
