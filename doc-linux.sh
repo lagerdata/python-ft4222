95c3189897c00466f9dc670eab4508f5185b8080
Martin Gysel <me@bearsh.org>
2017-06-07 11:49:50 +0200

    move and improve doc script

---
+++ doc-linux.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+unamem=$(uname -m)
+
+case "$unamem" in
+        x86_64) platform="x86_64" ;;
+        i386|i686) platform="i386" ;;
+        armv6*|armv7*) platform="arm-v6-hf" ;;
+        *) echo "Libft4222 is not currently supported on '$unamem'."; exit 1 ;;
+esac
+
+if [[ -z $1 ]]; then
+	LD_LIBRARY_PATH=../linux/build-${platform}/. PYTHONPATH=.. make -C doc/ html
+else
+	LD_LIBRARY_PATH=../linux/build-${platform}/. PYTHONPATH=.. make -C doc/ $@
+fi
--- doc/doc-linux.sh
+++
@@ -1,1 +0,0 @@
-LD_LIBRARY_PATH=../linux/build-x86_64/. PYTHONPATH=.. make html
