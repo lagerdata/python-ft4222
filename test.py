9cb8c87b87234d2713a17f11af2eb7664b2887c5
Martin Gysel <me@bearsh.org>
2020-01-17 14:48:09 +0100

    remove some steps in test.py

--- test.py
+++ test.py
@@ -17,30 +17,13 @@ if nbDev <= 0:
     print("no devices found...")
     sys.exit(0)
 
-
 print("devices:")
 for i in range(nbDev):
     detail = ft4222.getDeviceInfoDetail(i, False)
     print(" - {}".format(detail))
     ftDetails.append(detail)
 
-#dev = ft4222.open_by_description(ftDetails[0]['description'])
-dev = ft4222.openByLocation(ftDetails[0]['location'])
+dev = ft4222.openByDescription('FT4222 A')
 print(dev)
 
-dev.i2cMaster_Init(250)
-
-res = dev.i2cMaster_Read(2, 1)
-print(res)
-
-res = dev.i2cMaster_Write(2, bytes([1,2,3,4]))
-print(res)
-
-res = dev.i2cMaster_Read(2, 1)
-print(res)
-
-res = dev.i2cMaster_Write(2, bytes([2]*7))
-print(res)
-
-res = dev.i2cMaster_Read(2, 4)
-print(res)
+dev.i2cMaster_Init(100)
