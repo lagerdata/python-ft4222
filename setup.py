dfe829ba4b34692e9f19f3dd83cff208c3b568dd
Martin Gysel <me@bearsh.org>
2021-10-26 10:07:44 +0200

    bump version to 1.3.0

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ft4222',
-    version='1.2.2',
+    version='1.3.0',
     author='Bearsh',
     author_email='me@bearsh.org',
     url='https://gitlab.com/msrelectronics/python-ft4222',
